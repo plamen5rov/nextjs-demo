04d6c1daaf56d39a305ba959941cd9bdc5418355
Plamen <biznetmen@gmail.com>
2023-02-05 17:33:07 +0200

    more routes added

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+
 function NewsPage() {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="news/news-one">News 1</Link>
+        </li>
+        <li>
+          <Link href="news/news-two">News 2</Link>
+        </li>
+        <li>
+          <Link href="news/news-three">News 3</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
